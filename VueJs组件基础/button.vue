e75e62b0205d3aafbe75c3f923cf8c5d4a4c1ae4
ZhuChao <2580500887@qq.com>
2019-09-26 11:37:19 +0800

    按钮例子

---
+++ VueJs组件基础/button.vue
@@ -0,0 +1,49 @@
+<template>
+    <button class="didi-btn" :class="{
+    'disanle':disabled,
+    'didi-btn-highlight':highlight
+    }" @click="handleClick($event)" ><slot></slot></button>
+</template>
+
+<script>
+    export default {
+        name: "dd-button",
+        props:{
+            disanle:Boolean,
+            highlight:Boolean
+        },
+        methods:{
+            handleClick:function (event) {
+                if (this.disabled){
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .didi-btn{
+        display: block;;
+        padding: 0;
+        width: 100%;
+        height:40px;
+    }
+
+    .didi-btn.active,.didi-btn:active{
+        background: #ebebeb;
+    }
+
+    .didi-btn.disable,.didi-btn:disabled{
+        background:#fafafa;
+    }
+
+    .didi-btn-hightlight{
+        background: #fa8919;
+        color: #fff;
+        border:none;
+    }
+    /*...*/
+
+</style>
